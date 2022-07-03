a0c893f4a643f90597766ed4c7c76ef05eee6ef9
Julija <julija.jurciene@gmail.com>
2022-07-03 11:02:58 +0300

    commit

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (5 > 10) {
+            System.out.println(5 > 10);
+        } else {
+            System.out.println(5 > 10);
+        }
     }
 }
